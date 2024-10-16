f24cfc32734a168254f0c0e4889a989bb69ee444
sowgro <tpoke.ferrari@gmail.com>
2024-10-15 23:20:33 -0400

    populate widgets with actual bookmarks

--- extension/src/Bookmark.tsx
+++ extension/src/Bookmark.tsx
@@ -1,12 +1,13 @@
 interface BookmarkProps {
     name: string;
     url: string;
+    icon: string;
 }
 
 function Bookmark(props: BookmarkProps) {
     return(
         <a className="bookmark" href={props.url}>
-            <img alt="Bookmark icon" src={""}></img>
+            <img alt="Bookmark icon" src={props.icon}></img>
             <span>{props.name}</span>
         </a>
     );
