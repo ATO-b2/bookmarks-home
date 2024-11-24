f92f1be605243e6c6829f09ac038d89cef4b740b
sowgro <tpoke.ferrari@gmail.com>
2024-11-24 14:51:08 -0500

    Improve icon sorting

--- extension/public/iconGrabber.js
+++ extension/public/iconGrabber.js
@@ -1,4 +1,4 @@
-export function getBrowser() {
+function getBrowser() {
     if (typeof browser === "undefined") {
         return chrome;
     } else {
@@ -11,12 +11,19 @@ const tagTypes = ["apple-touch-icon", "shortcut icon", "icon"]
 let x = Array.from(document.getElementsByTagName("link"))
     .filter(elem => tagTypes.includes(elem.rel))
     .sort((a, b) => {
-        let tagCompare = tagTypes.indexOf(a.rel) - tagTypes.indexOf(b.rel);
-        if (tagCompare !== 0) return tagCompare;
-        try { return Number(b.sizes[0].split('x')[0]) - Number(a.sizes[0].split('x')[0]); }
-        catch { return -1; }
-    })
-    .map(elem => elem.href);
+        function compareTags() {
+            return tagTypes.indexOf(a.rel) - tagTypes.indexOf(b.rel);
+        }
+        function compareSizes() {
+            try { return Number(b.sizes[0].split('x')[0]) - Number(a.sizes[0].split('x')[0]); }
+            catch { return -1; }
+        }
+
+        return compareSizes() || compareTags()
+    });
+
+console.log("found icons", x.map(elem => elem.outerHTML));
+x = x.map(elem => elem.href);
 
 getBrowser().runtime.sendMessage([window.location.href, x[0]]).catch(() => {
     console.log("failed to send message")
